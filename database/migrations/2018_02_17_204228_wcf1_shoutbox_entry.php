a59ecbc3745231b282eef1fc3b9ce2ea1a1fa49c
Quabit <chris@quabit.io>
2018-02-17 21:43:42 +0100

    Missing migration

---
+++ database/migrations/2018_02_17_204228_wcf1_shoutbox_entry.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Wcf1ShoutboxEntry extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('wcf1_shoutbox_entry')) {
+
+            Schema::table('wcf1_shoutbox_entry', function (Blueprint $table) {
+                $table->boolean('syncWithExternalServices')->default(1);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('wcf1_shoutbox_entry', function (Blueprint $table) {
+            $table->dropColumn('syncWithExternalServices');
+        });
+    }
+}
